67729d885d9a61e193afaaacb1cc948f0028f6d9
Nostrademous <nostrademous@hotmail.com>
2020-08-26 23:33:31 -0400

    changed default upload image location

--- trivianator/trivia/models.py
+++ trivianator/trivia/models.py
@@ -564,7 +564,7 @@ class Question(models.Model):
                                  blank=True,
                                  null=True, on_delete=models.CASCADE)
 
-    figure = models.ImageField(upload_to='uploads/%Y/%m/%d',
+    figure = models.ImageField(upload_to='uploads/quiz_images',
                                blank=True,
                                null=True,
                                verbose_name=_("Figure"))
